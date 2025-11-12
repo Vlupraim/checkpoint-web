5e3c8dcfc8c2efb1792102b711c4fa1250c7ed9c
Vlue <dzdzzavando@gmail.com>
2025-11-11 23:32:30 -0300

    fix: Corregir errores HTTP 500 y eliminar emojis corruptos

--- checkpoint-web/Pages/Calidad/Historial.cshtml.cs
+++ checkpoint-web/Pages/Calidad/Historial.cshtml.cs
@@ -1,13 +1,27 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Authorization;
+using checkpoint_web.Services;
+using checkpoint_web.Models;
 
 namespace checkpoint_web.Pages.Calidad
 {
   [Authorize(Roles = "ControlCalidad")]
     public class HistorialModel : PageModel
     {
-        public void OnGet()
+    private readonly ICalidadService _calidadService;
+
+        public HistorialModel(ICalidadService calidadService)
+   {
+     _calidadService = calidadService;
+        }
+
+    public IEnumerable<CalidadLiberacion> Historial { get; set; } = new List<CalidadLiberacion>();
+        public Dictionary<string, int> Estadisticas { get; set; } = new();
+
+        public async Task OnGetAsync()
   {
+            Historial = await _calidadService.GetHistorialCalidadAsync(100);
+Estadisticas = await _calidadService.GetEstadisticasAsync();
         }
     }
 }
