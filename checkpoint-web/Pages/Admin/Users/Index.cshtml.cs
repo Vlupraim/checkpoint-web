f72d2c6792ad813e661024147373a5209fc45cda
Vlue <dzdzzavando@gmail.com>
2025-12-02 20:56:12 -0300

    Refactor: Unificar Index y Usuarios con UsuariosModel
    - Index.cshtml ahora hereda de UsuariosModel
    - Eliminar duplicación de código
    - Consistencia entre página standalone y fragment SPA
    - Mismo formato de datos (tupla con Id, Email, Nombre, Roles)

--- checkpoint-web/Pages/Admin/Users/Index.cshtml.cs
+++ checkpoint-web/Pages/Admin/Users/Index.cshtml.cs
@@ -5,28 +5,11 @@ using checkpoint_web.Models;
 
 namespace checkpoint_web.Pages.Admin.Users
 {
- [Authorize(Roles = "Administrador")]
- public class IndexModel : PageModel
- {
- private readonly UserManager<ApplicationUser> _userManager;
- private readonly RoleManager<IdentityRole> _roleManager;
- public IndexModel(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
- {
- _userManager = userManager;
- _roleManager = roleManager;
- }
-
- public IList<ApplicationUser> Users { get; set; } = new List<ApplicationUser>();
- public Dictionary<string, IList<string>> RolesForUser { get; set; } = new Dictionary<string, IList<string>>();
-
- public async Task OnGetAsync()
- {
- Users = _userManager.Users.ToList();
- foreach (var u in Users)
- {
- var roles = await _userManager.GetRolesAsync(u);
- RolesForUser[u.Id] = roles;
- }
- }
- }
+    [Authorize(Roles = "Administrador")]
+    public class IndexModel : checkpoint_web.Pages.Fragments.UsuariosModel
+    {
+        public IndexModel(UserManager<ApplicationUser> userManager) : base(userManager)
+        {
+        }
+    }
 }
